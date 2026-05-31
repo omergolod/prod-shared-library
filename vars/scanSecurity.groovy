fc71f3b28eeadee62c66fcb013fc8db256560207
Ubuntu <ubuntu@ip-172-31-39-126.eu-west-1.compute.internal>
2026-05-31 12:28:36 +0000

    Fix: Implement proper closure-delegate block

--- vars/scanSecurity.groovy
+++ vars/scanSecurity.groovy
@@ -1,9 +1,4 @@
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
+def call(Map config) {
     echo "Starting security scans..."
     echo "Scanning target: ${config.scanTarget}"
     echo "Failure threshold set to: ${config.failThreshold}"
